9f7a5433a9e9f17d113c1e2ca34b9ccb53d1b37f
Razvan <40596596+RazvanRauta@users.noreply.github.com>
2025-03-19 14:08:08 +0100

    feat: update AppModule for middleware configuration

---
+++ src/app.middleware.ts
@@ -0,0 +1,39 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTPResponse');
+
+  use(
+    req: FastifyRequest['raw'],
+    res: FastifyReply['raw'],
+    next: () => void,
+  ): void {
+    const startAt = process.hrtime();
+    const { method, headers } = req;
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+
+      const diff = process.hrtime(startAt);
+      const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
+
+      const stringFormat = `"${method} - ${req['url']} - ${
+        headers.host
+      } - ${statusCode} - ${responseTime.toFixed(2)}ms" - ${
+        headers['user-agent']
+      } - ${Array.isArray(headers['X-Forwarded-For']) ? headers['X-Forwarded-For'].join(', ') : (headers['X-Forwarded-For'] ?? 'Untrusted')}`;
+
+      if (statusCode >= 500) {
+        this.logger.error(stringFormat);
+      } else if (statusCode >= 400) {
+        this.logger.warn(stringFormat);
+      } else {
+        this.logger.log(stringFormat);
+      }
+    });
+
+    next();
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
 import { createDbConfig } from './database/create-db-config';
 import appConfig from './config/app.config';
 import databaseConfig from './database/config/database.config';
+import { CreateRequestContext, MikroORM } from '@mikro-orm/postgresql';
+import { LoggerMiddleware } from './app.middleware';
 
 @Module({
   imports: [
@@ -20,7 +20,17 @@ import databaseConfig from './database/config/database.config';
       inject: [ConfigService],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  @CreateRequestContext()
+  async onModuleInit() {
+    await this.orm.getMigrator().up();
+  }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, MikroOrmMiddleware).forRoutes('*');
+  }
+}
